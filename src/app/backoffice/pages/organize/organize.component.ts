74c7695b369f16f8bbe0c21d498a2a8aae0df1b4
Cristian macha <c71917455@gmail.com>
2021-07-03 20:10:41 -0500

    avance

--- src/app/backoffice/pages/organize/organize.component.ts
+++ src/app/backoffice/pages/organize/organize.component.ts
@@ -5,8 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { MateriasO, Organize } from '@core/models/materia.model';
-import { AcademicPlanService } from '@core/services/academic-plan.service';
+import { Organize } from '@core/models/materia.model';
 import { HorarioService } from '@core/services/horario.service';
 
 export interface ISemana {
@@ -30,6 +29,9 @@ export class OrganizeComponent implements OnInit {
   matricula: string;
   semana: ISemana;
 
+  listaDeMaterias = [];
+  materiasObligadas = [];
+
   showTable: boolean = false;
 
   constructor(
@@ -46,40 +48,30 @@ export class OrganizeComponent implements OnInit {
   ngOnInit(): void {}
 
   planificar() {
+    console.log(this.materiasObligadas);
+    
+    this.materiasObligadas.forEach(element => {
+      this.materiasO.push({mat_id: element.mat_id, NRC: element.NRC})
+    });
+
     this.organize = {
       ...this.organizeForm.value,
       matricula: this.matricula,
       materias_obligadas: this.materiasO,
     };
+    console.log(this.organize);
+    
     this._horarioService.enviar(this.organize).subscribe(
       (data: any) => {
-        this.semana = data;
-        console.log(data);
+        this.semana = data.horario;
+        this.validarMaterias(data.lista_horario);
+        console.log('data', data);
         
-        this.quitarMateria();
+        // this.quitarMateria();
         this.showTable = true;
       },
       (error) => console.error(error)
     );
-  }
-
-  quitarMateria() {
-
-    if (this.semana?.Lunes['7:00']) {
-      console.log(this.semana?.Lunes);
-      const mat = this.materiasO.filter(m => m.mat_id==this.semana?.Lunes['07:00']?.mat_id)
-      if(mat.length == 0) {
-        this.materiasO.push({
-          mat_id: this.semana?.Lunes['07:00']?.mat_id,
-          NRC: this.semana?.Lunes['07:00']?.NRC,
-        });
-      }
-    }
-
-    // const materia = new MateriasO();
-    // materia.mat_id = mat_id;
-    // materia.NRC = NRC;
-    // this.materiasO.push(materia);
   }
 
   aceptarPlan() {
@@ -87,5 +79,27 @@ export class OrganizeComponent implements OnInit {
       (data) => console.log(data),
       (error) => console.error(error)
     );
+  }
+
+  validarMaterias(lista: any) {
+    for (let index = 0; index < lista.length; index++) {
+      const element = lista[index];
+      const existe = this.listaDeMaterias.some(
+        (m) => m.mat_id === element.mat_id
+      );
+      if (!existe) {
+        this.listaDeMaterias.push(element);
+      }
+    }
+  }
+
+  seleccionarMaterias(index: number, materia: any) {
+    this.listaDeMaterias.splice(index, 1);
+    this.materiasObligadas.push(materia);
+  }
+
+  quitarMateria(index: number, materia: any) {
+    this.materiasObligadas.splice(index, 1);
+    this.listaDeMaterias.push(materia);
   }
 }
